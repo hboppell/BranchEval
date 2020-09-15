7da7df79815e25814ab0eca079b3427ebdd1c70c
Harrison Boppell <hboppell@yahoo.com>
2020-09-15 14:42:59 -0700

    committing staged changes

---
+++ BranchEval.Tests/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BranchEval.Controllers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BranchEval.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        //Test if "api/user" endpoint with no actual username sends the user to the correct page
+        [TestMethod]
+        public void Get_ShouldReturnDefaultPage()
+        {
+            var controller = new UserController();
+            string result = controller.Get();
+            StringAssert.Equals(result, "Please add the username to the endpoint to get information for a specific GitHub user.");
+        }
+
+        //Test if "api/user/octocat2" correctly returns the "no user found" error message
+        [TestMethod]
+        public void GetUser_ShouldReturnErrorForInvalidUser()
+        {
+            var controller = new UserController();
+            JToken result = controller.GetUser("octocat2").Result;
+            StringAssert.Equals(result.ToString(), "No GitHub user found with the username 'octocat2'.");
+        }
+
+        //Test if "api/user/octocat" correctly returns octocat's user info
+        [TestMethod]
+        public void GetUser_ShouldReturnValidUser()
+        {
+            var controller = new UserController();
+            JToken result = controller.GetUser("octocat").Result;
+            Assert.IsFalse(result.ToString().Contains("No GitHub user found with the username 'octocat2'."));
+        }
+
+        //Test if "api/user/repos/octocat2" correctly returns the "no user found" error message
+        [TestMethod]
+        public void GetUserRepo_ShouldReturnErrorForInvalidUser()
+        {
+            var controller = new UserController();
+            JToken result = controller.GetUserRepo("octocat2").Result;
+            StringAssert.Equals(result.ToString(), "No GitHub user found with the username 'octocat2'.");
+        }
+
+        //Test if "api/user/repos/octocat" correctly returns octocat's repos
+        [TestMethod]
+        public void GetUserRepo_ShouldReturnValidUser()
+        {
+            var controller = new UserController();
+            JToken result = controller.GetUserRepo("octocat").Result;
+            Assert.IsFalse(result.ToString().Contains("No GitHub user found with the username 'octocat2'."));
+        }
+    }
+}
---
+++ BranchEval/Controllers/UserController.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BranchEval.Controllers
+{
+    
+    [RoutePrefix("api/user")]
+    public class UserController : ApiController
+    {
+        public string baseUrl = "https://api.github.com/users";
+
+        //Very simple caching mechanism so each username only has to be queried once per endpoint
+        public static Dictionary<string, JToken> userCache = new Dictionary<string, JToken>();
+        public static Dictionary<string, JToken> userRepoCache = new Dictionary<string, JToken>();
+
+        //If the user calls "api/user" without the actual username, they will receive this message back (this should also be the default page when you run the code)
+        [HttpGet]
+        public string Get()
+        {
+            return "Please add the username to the endpoint to get information for a specific GitHub user.";
+        }
+
+
+        //Call "api/user/{user}" to get the info for a specific GitHub user
+        [HttpGet]
+        [Route("{user}")]
+        public async Task<JToken> GetUser(string user)
+        {
+            //If the API call has already been made, no need to make it again
+            if (userCache.ContainsKey(user)) return userCache[user];
+
+            //Set up call to GitHub API user endpoint
+            RestClient client = new RestClient(baseUrl);
+            RestRequest request = new RestRequest(user);
+
+            //Make the REST call
+            var response = await client.ExecuteGetAsync(request);
+
+            //Determine what error text to display to the user based on the error returned from GitHub.
+            if (response.StatusCode == HttpStatusCode.NotFound) return GetErrorResponse(response.StatusCode, user);
+
+            string responseString = JsonConvert.SerializeObject(response.Content, Formatting.Indented);
+
+            //Add the response to the cache
+            userCache.Add(user, JValue.Parse(response.Content));
+
+            //return JToken.Parse(response.Content).ToString(Formatting.Indented);
+            return JValue.Parse(response.Content);
+        }
+
+        //Call "api/user/{user}/repos" to get the repose for a specific GitHub user
+        [HttpGet]
+        [Route("{user}/repos")]
+        public async Task<JToken> GetUserRepo(string user)
+        {
+            if (userRepoCache.ContainsKey(user)) return userRepoCache[user];
+
+            //Set up call to GitHub API user/repos endpoint
+            RestClient client = new RestClient(baseUrl);
+            RestRequest request = new RestRequest(user + "/repos");
+
+            var response = await client.ExecuteGetAsync(request);
+
+            //Determine what error text to display to the user based on the error returned from GitHub.
+            if (response.StatusCode == HttpStatusCode.NotFound) return GetErrorResponse(response.StatusCode, user);
+
+            //Add the response to the cache, so that this specific username will not have to be queried again
+            userRepoCache.Add(user, JValue.Parse(response.Content));
+
+            return JValue.Parse(response.Content);
+        }
+
+
+        //The error handling can easily be expanded upon if necessary
+        public string GetErrorResponse(HttpStatusCode statusCode, string user)
+        {
+            switch (statusCode)
+            {
+                case HttpStatusCode.NotFound: return "No GitHub user found with the username '" + user + "'.";
+            }
+
+            return "An error has occurred. Please try again later.";
+        }
+    }
+}
